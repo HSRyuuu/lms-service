f751c03f68b25867626055d8320ce346e0b22e71
HyunSik Ryu <hsryu123@naver.com>
2023-09-07 22:57:19 +0900

    로그인 히스토리 엔티티 생성

---
+++ src/main/java/com/zerobase/lms/entity/loginhistory/LoginHistory.java
@@ -0,0 +1,29 @@
+package com.zerobase.lms.entity.loginhistory;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+@Entity
+public class LoginHistory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String userId;
+    private LocalDateTime loginDt;
+    private String accessIp;
+    private String userAgent;
+
+}
