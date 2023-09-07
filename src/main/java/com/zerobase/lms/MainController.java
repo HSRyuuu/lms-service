34bc77c8a9f97712651546b2dc548265505fa7cf
HyunSik Ryu <hsryu123@naver.com>
2023-09-07 17:58:10 +0900

    MailComponents - 메일 테스트 Gmail mail sender

--- src/main/java/com/zerobase/lms/MainController.java
+++ src/main/java/com/zerobase/lms/MainController.java
@@ -1,6 +1,7 @@
 package com.zerobase.lms;
 
 
+import com.zerobase.lms.service.mail.MailComponents;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,17 +10,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class MainController {
 
-    
+    private final MailComponents mailComponents;
+
     @RequestMapping("/")
     public String index() {
-        
-        /*
-        String email = "satcop@naver.com";
+        String email = "hsryu123@naver.com";
         String subject = " 안녕하세요. 제로베이스 입니다. ";
         String text = "<p>안녕하세요.</p><p>반갑습니다.</p>";
-        
+
         mailComponents.sendMail(email, subject, text);
-        */
+
         
         return "index";
     }
