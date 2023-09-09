a620532c23cf80f2efd507fcd206011cfa7fd369
HyunSik Ryu <hsryu123@naver.com>
2023-09-09 21:39:51 +0900

    카테고리 CRUD

---
+++ src/main/java/com/zerobase/lms/admin/mapper/CategoryMapper.java
@@ -0,0 +1,11 @@
+package com.zerobase.lms.admin.mapper;
+
+
+import com.zerobase.lms.admin.dto.CategoryDto;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface CategoryMapper {
+}
